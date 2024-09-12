571f14d3874e3ceee4c6760010540a59520d37dc
p-bissoni <patrickbissoni20073@gmail.com>
2024-09-11 21:48:51 -0300

    Código inicial
    Código inicial de uma calculadora usando Pyhton

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # trabalhodevops
+
+# Calculadora 
+Projeto de uma calculadora básica em Python, com operações de soma, subtração, multiplicação e divisão.
+
+## Como Usar
+Execute o arquivo `calculadora.py` no terminal para usar a calculadora:
+
+`python main.py`
---
+++ main.py
@@ -0,0 +1,28 @@
+# calculadora.py
+
+# Menu da calculadora
+def menu():
+    print("Selecione a operação:")
+    print("1. Adição")
+    print("2. Subtração")
+    print("3. Multiplicação")
+    print("4. Divisão")
+
+# Programa principal
+while True:
+    menu()
+    
+    # Recebe a entrada do usuário
+    escolha = input("Digite sua escolha (1/2/3/4) ou 'sair' para encerrar: ")
+
+    if escolha == 'sair':
+        print("Fechando a Calculadora.")
+        break
+
+    if escolha in ['1', '2', '3', '4']:
+        num1 = float(input("Digite o primeiro número: "))
+        num2 = float(input("Digite o segundo número: "))
+
+    else:
+        print("Escolha inválida. Tente novamente.")
+
