05af69f49b4e32eaca5264441c082e58ee21bdcb
p-bissoni <patrickbissoni20073@gmail.com>
2024-09-11 22:15:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -41,6 +41,18 @@ while True:
         num1 = float(input("Digite o primeiro número: "))
         num2 = float(input("Digite o segundo número: "))
 
+        if escolha == '1':
+            print(f"{num1} + {num2} = {adicionar(num1, num2)}")
+
+        elif escolha == '2':
+            print(f"{num1} - {num2} = {subtrair(num1, num2)}")
+
+        elif escolha == '3':
+            print(f"{num1} * {num2} = {multiplicar(num1, num2)}")
+
+        elif escolha == '4':
+            print(f"{num1} / {num2} = {dividir(num1, num2)}")
+
     else:
         print("Escolha inválida. Tente novamente.")
 
