2a5f1b7c1d001e297df7b98dee7d92c140701d62
p-bissoni <patrickbissoni20073@gmail.com>
2024-09-13 13:26:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,29 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# Mensagem de boas-vindas
 @app.get("/")
 def read_root():
+    # Mensagem de boas-vindas
     return {"mensagem": "Bem-vindo!"}
 
-# Realiza adição de dois números
 @app.get("/adicionar")
 def adicionar(x: float, y: float):
+    # Realiza adição de dois números
     return {"resultado": x + y}
 
-# Realiza subtração de dois números
 @app.get("/subtrair")
 def subtrair(x: float, y: float):
+    # Realiza subtração de dois números
     return {"resultado": x - y}
 
-# Realiza multiplicação de dois números
 @app.get("/multiplicar")
 def multiplicar(x: float, y: float):
+    # Realiza multiplicação de dois números
     return {"resultado": x * y}
 
-#Realiza divisão de dois números, se o divisor for zero, retorna um mensagem de erro
 @app.get("/dividir")
 def dividir(x: float, y: float):
+    #Realiza divisão de dois números, se o divisor for zero, retorna um mensagem de erro
     if y == 0:
         return "Erro: divisão por zero!"
     return {"resultado": x / y}
