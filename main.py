d3756750727915e2ce290d2f906c508db06682b0
p-bissoni <patrickbissoni20073@gmail.com>
2024-09-13 13:20:34 -0300

    Mudança arquivos
    Mudança no arquivo main e requirements.txt

--- main.py
+++ main.py
@@ -1,58 +1,31 @@
 # calculadora.py
+from fastapi import FastAPI
 
-# Adicionar dois números
-def adicionar(x, y):
-    return x + y
+app = FastAPI()
 
-# Subtrair dois números
-def subtrair(x, y):
-    return x - y
+@app.get("/")
+def read_root():
+    # Mensagem de boas-vindas
+    return {"mensagem": "Bem-vindo!"}
 
-# Multiplicar dois números
-def multiplicar(x, y):
-    return x * y
+@app.get("/adicionar")
+def adicionar(x: float, y: float):
+    # Realiza adição de dois números
+    return {"resultado": x + y}
 
-# Dividir dois números
-def dividir(x, y):
+@app.get("/subtrair")
+def subtrair(x: float, y: float):
+    # Realiza subtração de dois números
+    return {"resultado": x - y}
+
+@app.get("/multiplicar")
+def multiplicar(x: float, y: float):
+    # Realiza multiplicação de dois números
+    return {"resultado": x * y}
+
+@app.get("/dividir")
+def dividir(x: float, y: float):
+    #Realiza divisão de dois números, se o divisor for zero, retorna um mensagem de erro
     if y == 0:
         return "Erro: divisão por zero!"
-    return x / y
-
-# Menu da calculadora
-def menu():
-    print("Selecione a operação:")
-    print("1. Adição")
-    print("2. Subtração")
-    print("3. Multiplicação")
-    print("4. Divisão")
-
-# Programa principal
-while True:
-    menu()
-    
-    # Recebe a entrada do usuário
-    escolha = input("Digite sua escolha (1/2/3/4) ou 'sair' para encerrar: ")
-
-    if escolha == 'sair':
-        print("Fechando a Calculadora.")
-        break
-
-    if escolha in ['1', '2', '3', '4']:
-        num1 = float(input("Digite o primeiro número: "))
-        num2 = float(input("Digite o segundo número: "))
-
-        if escolha == '1':
-            print(f"{num1} + {num2} = {adicionar(num1, num2)}")
-
-        elif escolha == '2':
-            print(f"{num1} - {num2} = {subtrair(num1, num2)}")
-
-        elif escolha == '3':
-            print(f"{num1} * {num2} = {multiplicar(num1, num2)}")
-
-        elif escolha == '4':
-            print(f"{num1} / {num2} = {dividir(num1, num2)}")
-
-    else:
-        print("Escolha inválida. Tente novamente.")
-
+    return {"resultado": x / y}
